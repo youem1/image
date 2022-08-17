bb589ff5d8cde5c48790133799115fad9960859f
you em <youem850@gmail.com>
2022-08-18 00:55:17 +0430

    better

--- front/sw.js
+++ front/sw.js
@@ -7,6 +7,7 @@ const urlsToCache = [
     '/manifest.json',
     '/',
     '/sw.js'
+
 ];
 
 //install sw
@@ -26,23 +27,24 @@ self.addEventListener('install', (event) => {
 //listen for request
 self.addEventListener('fetch', (event) => {
     console.log('[Service Worker]: fetched');
+
     event.respondWith(
         fetch(event.request)
         .then((res) => {
             // console.log('5555', res)
-            if (res) {
-                return caches.open(CACHE_NAME)
-                    .then(function(cache) {
-                        cache.put(event.request.url, res.clone());
-                        return res;
-                    })
 
-            }
+            return caches.open(CACHE_NAME)
+                .then(function(cache) {
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                })
+
+
         }).catch((err) => {
             // console.log('[Service Worker]:fetch data is failure cache is find')
             return (caches.match(event.request)
                 .then((res) => {
-                    console.log('[Service Worker]: find file in cache store');
+                    // console.log('[Service Worker]: find file in cache store');
                     return res;
                 }))
 
