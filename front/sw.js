0f3814b70939997bd2811f17916c16a0487395fa
you em <youem850@gmail.com>
2022-08-19 10:23:28 +0430

    better

--- front/sw.js
+++ front/sw.js
@@ -1,11 +1,16 @@
 const CACHE_NAME = "version1";
 const urlsToCache = [
-
+    "static/js/main.5e0ba60c.js",
+    "/static/css/main.44fb2667.css",
+    "/static/css/main.44fb2667.css.map",
+    '/static/js/main.5e0ba60c.js.map',
     '/manifest.json',
     '/',
     '/index.html',
     '/sw.js',
-
+    'https://fonts.googleapis.com/css2?family=Nunito&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&family=Vazirmatn:wght@100;200;300;400;500;600;900&display=swap',
+    'https://fonts.gstatic.com/s/vazirmatn/v6/Dxxo8j6PP2D_kU2muijlGMWWIGroe7ll.woff2',
+    'https://fonts.gstatic.com/s/vazirmatn/v6/Dxxo8j6PP2D_kU2muijlHcWWIGroew.woff2',
     // 'https://s24.picofile.com/file/8451959984/ic_launcher.bmp',
     // 'https://s25.picofile.com/file/8451911668/BeFunky_photo.png'
 
