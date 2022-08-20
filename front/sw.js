689120ac31f2e16e3727b5365d9d6d2858852468
you em <youem850@gmail.com>
2022-08-20 14:27:45 +0430

    better

--- front/sw.js
+++ front/sw.js
@@ -18,35 +18,27 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     console.log('[Service Worker]: fetched');
     // event.respondWith(
-    //         fetch(event.request)
-    //         .then(res => {
-    //             if (res) {
+    //     fetch(event.request)
+    //     .then(res => {
+    //         return caches.open(CACHE_NAME)
+    //             .then(cache => {
+    //                 cache.put(event.request.url, res.clone());
+    //                 console.log('[Service Worker]:fetching.')
+    //                 return res;
+    //             })
+    //     }).catch(err => {
+    //         caches.match(event.request)
+    //             .then(res => {
+    //                 if (res) {
+    //                     console.log('[Service Worker]:caching.')
+    //                     return res;
 
-    //                 console.log('[Service Worker]:fetching.', res);
-    //                 return caches.open(CACHE_NAME)
-    //                     .then((cache) => {
-    //                         cache.put(event.request.url, res.clone());
-    //                         return res;
-
-    //                     })
-    //             }
-
-
-
-    //         }).catch(err => {
-    //             alert('برای دریافت اخرین نسخه برنامه لطفا اتصال خود را بررسی کنید')
-    //             caches.match(event.request)
-    //                 .then(res => {
-    //                     if (res) {
-    //                         console.log('[Service Worker]:caching.')
-    //                         return res;
-
-    //                     } else {
-    //                         console.log('[Service Worker]:can not fetch and caching!')
-    //                     }
-    //                 })
-    //         })
-    //     )
+    //                 } else {
+    //                     console.log('[Service Worker]:can not fetch and caching!')
+    //                 }
+    //             })
+    //     })
+    // )
     event.respondWith(
         caches.match(event.request)
         .then(function(response) {
