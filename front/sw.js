45bb4bf327547ca9e45c1bf5e68e951b3857aa10
you em <youem850@gmail.com>
2022-08-22 11:08:57 +0430

    better

--- front/sw.js
+++ front/sw.js
@@ -18,27 +18,27 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     console.log('[Service Worker]: fetched');
     // event.respondWith(
-    //     fetch(event.request)
-    //     .then(res => {
-    //         return caches.open(CACHE_NAME)
-    //             .then(cache => {
-    //                 cache.put(event.request.url, res.clone());
-    //                 console.log('[Service Worker]:fetching.')
-    //                 return res;
-    //             })
-    //     }).catch(err => {
-    //         caches.match(event.request)
-    //             .then(res => {
-    //                 if (res) {
-    //                     console.log('[Service Worker]:caching.')
+    //         fetch(event.request)
+    //         .then(res => {
+    //             return caches.open(CACHE_NAME)
+    //                 .then(cache => {
+    //                     cache.put(event.request.url, res.clone());
+    //                     console.log('[Service Worker]:fetching.')
     //                     return res;
+    //                 })
+    //         }).catch(err => {
+    //             caches.match(event.request)
+    //                 .then(res => {
+    //                     if (res) {
+    //                         console.log('[Service Worker]:caching.')
+    //                         return res;
 
-    //                 } else {
-    //                     console.log('[Service Worker]:can not fetch and caching!')
-    //                 }
-    //             })
-    //     })
-    // )
+    //                     } else {
+    //                         console.log('[Service Worker]:can not fetch and caching!')
+    //                     }
+    //                 })
+    //         })
+    //     )
     event.respondWith(
         caches.match(event.request)
         .then(function(response) {
